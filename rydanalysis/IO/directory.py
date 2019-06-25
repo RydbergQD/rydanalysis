33a9d3490e077fe4a96f94b6db27047269027c57
titusfranz <titus.franz@web.de>
2019-06-25 14:01:13 +0200

    read csv of old analysis

--- rydanalysis/IO/directory.py
+++ rydanalysis/IO/directory.py
@@ -5,7 +5,8 @@ from yaml import dump
 from shutil import copytree, copyfile, rmtree
 
 
-def dir_to_dict(path: str, include_files=False):
+# noinspection PyTypeChecker
+def dir_to_dict(path: str, include_files='all'):
     directory = {}
 
     for root, dirs, filenames in os.walk(path):
@@ -14,16 +15,51 @@ def dir_to_dict(path: str, include_files=False):
 
         if dirs:
             for d in dirs:
-                # noinspection PyTypeChecker
                 directory[dn].append(dir_to_dict(path=join(path, d), include_files=include_files))
 
-            if include_files:
-                for f in filenames:
-                    directory[dn].append(f)
-        elif include_files:
+            if include_files is not 'all':
+                len_filenames = len(filenames)
+                filenames = filenames[:include_files]
+                if len_filenames > include_files:
+                    filenames.append(r'...')
+            for f in filenames:
+                directory[dn].append(f)
+        else:
+            if include_files is not 'all':
+                len_filenames = len(filenames)
+                filenames = filenames[:include_files]
+                if len_filenames > include_files:
+                    filenames.append(r'...')
             directory[dn] = filenames
 
         return directory
+
+
+class GetterWithItem:
+    def __init__(self, f_get_item):
+        self.f_get_item = f_get_item
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return WithItem(self.f_get_item, instance)
+
+
+class WithItem:
+    def __init__(self, fct, instance):
+        self.fct = fct
+        self.instance = instance
+
+    def __getitem__(self, item):
+        return self.fct(self.instance, item)
+
+    def __delitem__(self, key):
+        folder = join(self.instance.path, list(self.instance.keys())[key])
+        if os.path.isdir(folder):
+            return rmtree(folder)
+        else:
+            return os.remove(folder)
 
 
 class Directory(MutableMapping):
@@ -62,6 +98,11 @@ class Directory(MutableMapping):
     def __iter__(self):
         for key in os.listdir(self.path):
             folder = join(self.path, key)
+            yield folder
+
+    def iter_dirs(self):
+        for key in os.listdir(self.path):
+            folder = join(self.path, key)
             if isdir(folder):
                 yield folder
 
@@ -82,11 +123,16 @@ class Directory(MutableMapping):
     def __repr__(self):
         return "directory: " + self.path
 
-    def structure(self, include_files=False):
+    def structure(self, include_files='all'):
         print(dump(dir_to_dict(self.path, include_files)))
 
     def rmtree(self):
         rmtree(self.path)
+
+    @GetterWithItem
+    def iloc(self, index):
+        key = list(self.keys())[index]
+        return self[key]
 
 
 class File:
--- rydanalysis/IO/old_structure.py
+++ rydanalysis/IO/old_structure.py
@@ -1,6 +1,7 @@
 from rydanalysis.IO.directory import Directory
 
 import pandas as pd
+import os
 from os.path import basename, join
 from tqdm import tqdm_notebook as tqdm
 
@@ -36,12 +37,16 @@ class OldStructure(Directory):
         return self['Voltages'][tmstp.strftime('voltages_' + strftime + '.xml')]
 
     def create_new_from_tmstp(self, path, tmstp):
-        new = Directory(join(path, tmstp.strftime(strftime)))
+        new = Directory(join(path, tmstp.strftime(strftime), 'exp_data'))
         new['variable.txt'] = self.get_variable(tmstp)
         new['exp_seq.xml'] = self.get_exp_seq(tmstp)
         new['image.fits'] = self.get_fits(tmstp)
         new['scope_trace.csv'] = self.get_scope_trace(tmstp)
         new['voltage.xml'] = self.get_voltage(tmstp)
+        old_la = self.get_old_la_from_tmstp(tmstp)
+        dir_analysis = join(path, tmstp.strftime(strftime), 'analysis')
+        os.makedirs(dir_analysis)
+        old_la.to_csv(join(dir_analysis, 'old_la.csv'))
         return new
 
     def create_new(self, path):
@@ -51,3 +56,17 @@ class OldStructure(Directory):
                 del new[tmstp.strftime(strftime)]
             self.create_new_from_tmstp(path, tmstp)
         return new
+
+    @property
+    def old_la(self):
+        file = self['FITS Files']['00_all_fit_results.csv']
+        old_la = pd.read_csv(file.path, index_col=0)
+        old_la.rename(columns=lambda x: x.replace(' ', ''), inplace=True)
+        old_la.set_index('time', inplace=True)
+        del old_la['Index']
+        return old_la
+
+    def get_old_la_from_tmstp(self, tmstp):
+        time = tmstp.hour * 60*60 + tmstp.minute * 60 + tmstp.second
+        old_la = self.old_la
+        return old_la.loc[time]
