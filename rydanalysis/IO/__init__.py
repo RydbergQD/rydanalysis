f31d7dcc071d0afea10ad5e66c32cc1ec9b5dda1
titusfranz <titus.franz@web.de>
2019-07-12 17:50:58 +0200

    combine os with h5 functionality

--- rydanalysis/IO/__init__.py
+++ rydanalysis/IO/__init__.py
@@ -2,4 +2,3 @@ from rydanalysis.IO.os import *
 from rydanalysis.IO.old_structure import *
 from rydanalysis.IO.single_shot import *
 from rydanalysis.IO.exp_sequence import *
-from rydanalysis.IO.h5 import *
--- rydanalysis/IO/os.py
+++ rydanalysis/IO/os.py
@@ -1,22 +1,23 @@
 import os
-from os.path import join, basename, isdir, isfile
-from collections import MutableMapping
+from os.path import join, basename, isdir, isfile, splitext
+from collections import MutableMapping, Iterable
 from yaml import dump
 from shutil import copytree, copyfile, rmtree
+import h5py
 
 
-def load(path, h5_group=None):
+def load(path, h5_group=None, lazy=False):
     if h5_group is None:
-        return load_path(path)
+        return load_path(path, lazy=lazy)
     else:
-        return load_h5(path, h5_group)
+        return load_h5(path, h5_group, lazy=lazy)
 
 
-def load_path(path):
+def load_path(path, lazy=False):
     if isdir(path):
         return load_dir(path)
     elif isfile(path):
-        return load_file(path)
+        return load_file(path, lazy=lazy)
     else:
         raise KeyError(path)
 
@@ -27,8 +28,24 @@ def load_dir(path):
     return Directory(path)
 
 
-def load_file(path):
+def load_file(path, lazy=False):
+    file_extension = splitext(path)[1]
+    if file_extension == '.h5':
+        return H5File(path, lazy=lazy)
     return File(path)
+
+
+def load_h5(path, h5_key, lazy=False):
+    with h5py.File(path, 'r') as hf:
+        if isinstance(hf[h5_key], h5py.Group):
+            return H5Group(path, h5_key, lazy=lazy)
+        elif isinstance(hf[h5_key], h5py.Dataset):
+            if lazy:
+                return H5Dataset(path, h5_key)
+            else:
+                return hf[h5_key][()]
+        else:
+            raise NotImplementedError()
 
 
 def remove(path, h5_group=None):
@@ -51,34 +68,18 @@ def remove_h5(path, h5_group):
     raise NotImplementedError()
 
 
-# noinspection PyTypeChecker
-def dir_to_dict(path: str, include_files='all'):
-    directory = {}
+def tree(origin, include_files='all'):
+    tree_dict = {origin.__name__: []}
+    origin.lazy = True
 
-    for root, dirs, filenames in os.walk(path):
-        dn = os.path.basename(root)
-        directory[dn] = []
-
-        if dirs:
-            for d in dirs:
-                directory[dn].append(dir_to_dict(path=join(path, d), include_files=include_files))
-
-            if include_files is not 'all':
-                len_filenames = len(filenames)
-                filenames = filenames[:include_files]
-                if len_filenames > include_files:
-                    filenames.append(r'...')
-            for f in filenames:
-                directory[dn].append(f)
+    for key, item in origin.items():
+        if isinstance(item, Iterable):
+            # noinspection PyTypeChecker
+            tree_dict[origin.__name__].append(tree(item, include_files=include_files))
         else:
-            if include_files is not 'all':
-                len_filenames = len(filenames)
-                filenames = filenames[:include_files]
-                if len_filenames > include_files:
-                    filenames.append(r'...')
-            directory[dn] = filenames
+            tree_dict[origin.__name__].append(str(item))
 
-        return directory
+    return tree_dict
 
 
 class GetterWithItem:
@@ -110,11 +111,12 @@ class WithItem:
 
 class Directory(MutableMapping):
 
-    def __init__(self, path):
+    def __init__(self, path, lazy=False):
         if not isdir(path):
             os.makedirs(path)
         self.path = path
         self.__name__ = basename(path)
+        self.lazy = lazy
 
     def __getitem__(self, key):
         path = join(self.path, key)
@@ -134,9 +136,7 @@ class Directory(MutableMapping):
         remove_path(path)
 
     def __iter__(self):
-        for key in os.listdir(self.path):
-            path = join(self.path, key)
-            yield path
+        return iter(os.listdir(self.path))
 
     def _ipython_key_completions_(self):
         return os.listdir(self.path)
@@ -164,8 +164,11 @@ class Directory(MutableMapping):
     def __repr__(self):
         return "directory: " + self.path
 
+    def __str__(self):
+        return "directory: " + self.__name__
+
     def tree(self, include_files='all'):
-        print(dump(dir_to_dict(self.path, include_files)))
+        print(dump(tree(self, include_files)))
 
     def rmtree(self):
         rmtree(self.path)
@@ -179,6 +182,7 @@ class Directory(MutableMapping):
 class File:
     def __init__(self, path):
         self.path = path
+        self.__name__ = basename(path)
 
     def open(self):
         raise NotImplementedError("File type is not yet implemented")
@@ -186,7 +190,130 @@ class File:
     def __repr__(self):
         return "file: " + self.path
 
+    def __str__(self):
+        return "file: " + self.__name__
+
     @property
     def file_type(self):
         file_name = basename(self.path)
         return file_name.split('.')[-1]
+
+
+def h5_join(path, *paths):
+    return join(path, *paths).replace('\\', '/')
+
+
+class H5File(MutableMapping):
+
+    def __init__(self, path, lazy=False):
+        if not isfile(path):
+            with h5py.File(join(path, 'test2.h5'), 'w') as _:
+                pass
+        self.path = path
+        self.__name__ = basename(path)
+        self.lazy = lazy
+
+    def __getitem__(self, h5_key):
+        return load_h5(self.path, h5_key, lazy=self.lazy)
+
+    def __setitem__(self, key: str, data):
+        with h5py.File(self.path, 'r+') as hf:
+            hf[key] = data
+
+    def __delitem__(self, key):
+        with h5py.File(self.path, 'r+') as hf:
+            del hf[key]
+
+    def __iter__(self):
+        with h5py.File(self.path, 'r+') as hf:
+            keys = list(hf.keys())
+        return iter(keys)
+
+    def _ipython_key_completions_(self):
+        with h5py.File(self.path, 'r') as hf:
+            return list(hf.keys())
+
+    def __len__(self):
+        with h5py.File(self.path, 'r') as hf:
+            return len(hf)
+
+    def __repr__(self):
+        return "H5File: " + self.path
+
+    def __str__(self):
+        return "H5File: " + self.__name__
+
+    def tree(self, include_files='all'):
+        return print(dump(tree(self, include_files)))
+
+    def remove(self):
+        os.remove(self.path)
+
+    def create_group(self, name, track_order=None):
+        with h5py.File(self.path, 'r+') as hf:
+            hf.create_group(name, track_order)
+        return H5Group(self.path, name)
+
+
+class H5Group(MutableMapping):
+    def __init__(self, path, h5_key, lazy=False):
+        self.path = path
+        self.h5_key = h5_key
+        self.__name__ = basename(h5_key)
+        self.lazy = lazy
+
+    def __getitem__(self, key):
+        h5_key = h5_join(self.h5_key, key)
+        return load_h5(self.path, h5_key, self.lazy)
+
+    def __setitem__(self, key: str, data):
+        h5_key = h5_join(self.h5_key, key)
+        with h5py.File(self.path, 'r+') as hf:
+            hf[h5_key] = data
+
+    def __delitem__(self, key):
+        h5_key = h5_join(self.h5_key, key)
+        with h5py.File(self.path, 'r+') as hf:
+            del hf[h5_key]
+
+    def __iter__(self):
+        with h5py.File(self.path, 'r+') as hf:
+            keys = list(hf[self.h5_key].keys())
+        return iter(keys)
+
+    def _ipython_key_completions_(self):
+        with h5py.File(self.path, 'r') as hf:
+            return list(hf[self.h5_key].keys())
+
+    def __len__(self):
+        with h5py.File(self.path, 'r') as hf:
+            return len(hf[self.h5_key])
+
+    def __repr__(self):
+        return "H5Group: " + self.path + 'grp: ' + self.h5_key
+
+    def __str__(self):
+        return "H5Group: " + self.__name__
+
+    def tree(self, include_files='all'):
+        return print(dump(tree(self, include_files)))
+
+    def remove(self):
+        with h5py.File(self.path, 'r') as hf:
+            del hf[self.h5_key]
+
+    def create_group(self, name, track_order=None):
+        with h5py.File(self.path, 'r+') as hf:
+            hf[self.h5_key].create_group(name, track_order)
+        return H5Group(self.path, h5_join(self.h5_key, name))
+
+
+class H5Dataset:
+    def __init__(self, path, h5_key):
+        self.path = path
+        self.h5_key = h5_key
+        self.__name__ = basename(h5_key)
+
+    def __repr__(self):
+        with h5py.File(self.path) as hf:
+            return str(hf[self.h5_key])
