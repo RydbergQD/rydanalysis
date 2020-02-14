38e788fa8a0abc32b381e4fb702d00377fb09789
dgrims <grimshandl@physi.uni-heidelberg.de>
2020-02-14 14:44:25 +0100

    add Parameters.to_dataset, Dataset.to_parameters methods

--- rydanalysis/fitting/utils.py
+++ rydanalysis/fitting/utils.py
@@ -1,15 +1,18 @@
-from xarray import Dataset
+import xarray as xr
 from lmfit import Parameters
+from xarray.core.extensions import _register_accessor
+
+Parameters._accessors = dict()
 
 
-def params_to_dataset(params, par_prefix="", par_suffix="", dim_name='fit'):
-    return Dataset({
+def parameters_to_dataset(params, par_prefix="", par_suffix="", dim_name='fit'):
+    return xr.Dataset({
         par_prefix + p.name + par_suffix: (dim_name, [p.value, p.min, p.max, p.init_value, p.stderr, p.vary])
         for p in params.values()},
         coords={dim_name: (dim_name, ['value', 'min', 'max', 'init_value', 'stderr', 'vary'])})
 
 
-def dataset_to_params(ds):
+def dataset_to_parameters(ds):
     """
     xr.Dataset needs to be 1d, missing parameter specifications will be silently skipped
     :param ds:
@@ -25,3 +28,33 @@ def dataset_to_params(ds):
                 pass
         params.add(var, **par_dict)
     return params
+
+
+def register_parameters_accessor(name):
+    """
+     Register a custom accessor on lmfit.Parameters objects.
+    :param name: Name under which the accessor should be
+    registered. A warning is issued if this name conflicts with a preexisting attribute.
+    :return:
+    """
+    return _register_accessor(name, Parameters)
+
+
+@xr.register_dataset_accessor('to_parameters')
+class DatasetToParameters:
+    def __init__(self, ds):
+        self.ds = ds
+        pass
+
+    def __call__(self):
+        return dataset_to_parameters(self.ds)
+
+
+@register_parameters_accessor('to_dataset')
+class ParametersToDataset:
+    def __init__(self, params):
+        self.params = params
+        pass
+
+    def __call__(self):
+        return parameters_to_dataset(self.params)
