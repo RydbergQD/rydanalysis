43f5613637820350a9c84090b1bbbfb4bcc8a95c
titusfranz <franzt@physi.uni-heidelberg.de>
2021-05-20 21:44:15 +0200

    sfi

--- rydanalysis/data_analysis/sfi.py
+++ rydanalysis/data_analysis/sfi.py
@@ -20,8 +20,8 @@ class MagnCalculator:
         self.bins=bins
         self.states = rydberg_states
 
-        self.kde_48 = self.gaussian_kde(peak_0)
-        self.kde_49 = self.gaussian_kde(peak_1)
+        self.kde_0 = self.gaussian_kde(peak_0)
+        self.kde_1 = self.gaussian_kde(peak_1)
 
     @classmethod
     def from_peak_df(cls, peak_df, bw_method=5e-2, bins=np.linspace(0, 100, 101),
@@ -36,7 +36,7 @@ class MagnCalculator:
     def model_func(self, x, magn):
         a = (2*magn + 1)/2
         b = 1 - a
-        return a * self.kde_49(x) + b*self.kde_48(x)
+        return a * self.kde_1(x) + b*self.kde_0(x)
 
     @cached_property
     def model(self):
@@ -51,9 +51,9 @@ class MagnCalculator:
     def plot_calibration(self, ax=None):
         if ax is None:
             fig, ax = plt.subplots()
-        ax.plot(self.bins, self.kde_48(self.bins), color=self.state_0_color)
+        ax.plot(self.bins, self.kde_0(self.bins), color=self.state_0_color)
         ax.hist(self.peak_0, bins=self.bins, alpha=0.7, label=self.states[0], density=True, color=self.state_0_color)
-        ax.plot(self.bins, self.kde_49(self.bins), color=self.state_1_color)
+        ax.plot(self.bins, self.kde_1(self.bins), color=self.state_1_color)
         ax.hist(self.peak_1, bins=self.bins, alpha=0.7, label=self.states[1], density=True, color=self.state_1_color)
         ax.set_xlabel(r"arrival time [$\mu$s]")
         ax.set_ylabel("peak density [1/$\mu$s]")
@@ -81,8 +81,8 @@ class MagnCalculator:
         ax.plot(bins, result.eval(x=bins), label="best fit", color=self.data_color)
         magn = result.best_values["magn"]
         a = (2*magn + 1)/2
-        ax.plot(bins, a*self.kde_49(bins), label=self.states[1], color=self.state_1_color)
-        ax.plot(bins, (1 - a)*self.kde_48(bins), label=self.states[0], color=self.state_0_color)
+        ax.plot(bins, a*self.kde_1(bins), label=self.states[1], color=self.state_1_color)
+        ax.plot(bins, (1 - a)*self.kde_0(bins), label=self.states[0], color=self.state_0_color)
         print(f"magnetization = {magn}")
         ax.text(0.8, 0.8, f"magnetization = {magn}")
         ax.set_xlabel(r"arrival time [$\mu$s]")
@@ -102,8 +102,8 @@ class MagnCalculator:
             phase_fit = fit_results_to_dict(phase_fit)
         return phase_fit
 
-    def plot_phase_fit(self, peak_df, model=CosineModel(), params=None, ax=None):
-        phase_fit = self.get_phase_fit(peak_df, model, params, to_dict=False)
+    def plot_phase_fit(self, peak_df, model=CosineModel(), params=None, ax=None, freq=1/360):
+        phase_fit = self.get_phase_fit(peak_df, model, params, to_dict=False, freq=freq)
         if ax is None:
             fig, ax = plt.subplots()
         ax.errorbar(phase_fit.data.index, phase_fit.data, 1/phase_fit.weights, linestyle='', marker='o')
@@ -113,9 +113,9 @@ class MagnCalculator:
         ax.set_ylabel("magnetization")
         return ax
 
-    def get_amp_df(self, peak_df):
+    def get_amp_df(self, peak_df, freq=1/360):
         names = self.extract_names(peak_df)
-        amp_df = peak_df.groupby(names).progress_apply(self.get_phase_fit)
+        amp_df = peak_df.groupby(names).progress_apply(self.get_phase_fit, freq=freq)
         return amp_df
 
     def extract_names(self, peak_df):
@@ -128,18 +128,52 @@ class MagnCalculator:
         z_magn = peak_df.peak_time.groupby(names).progress_apply(self.get_magn)
         return z_magn.unstack()
 
-    def get_magn_df_ramsey(self, peak_df):
+    def get_magn_df_ramsey(self, peak_df, freq=1/360):
         peak_xy = peak_df.xs([1, 1], level=["InitON", "Roff"], drop_level=True)
-        magn_df = self.get_amp_df(peak_xy)
+        magn_df = self.get_amp_df(peak_xy, freq=freq)
         peak_z = peak_df.xs([1, 0], level=["InitON", "Roff"], drop_level=True)
         z_df = self.get_z_magn_df(peak_z)
         magn_df["z_magn"] = z_df["magn"]
         magn_df["z_magn_stderr"] = z_df["magn_stderr"]
         return magn_df
 
-    def get_magn_df(self, peak_xy, peak_z):
-        magn_df = self.get_amp_df(peak_xy)
+    def get_magn_df(self, peak_xy, peak_z, freq=1/360):
+        magn_df = self.get_amp_df(peak_xy, freq=freq)
         z_df = self.get_z_magn_df(peak_z)
         magn_df["z_magn"] = z_df["magn"]
         magn_df["z_magn_stderr"] = z_df["magn_stderr"]
         return magn_df
+
+
+class MagnCalculator3lvl(MagnCalculator):
+    state_2_color = "tab:brown"
+    def __init__(self, peak_0, peak_1, peak_2, bw_method=5e-2, bins=np.linspace(0, 100, 101),
+     rydberg_states=["56S", "56P", "55S"]):
+        super().__init__(peak_0, peak_1, bw_method=bw_method, bins=bins, rydberg_states=rydberg_states)
+
+        self.peak_2 = peak_2
+        self.kde_2 = self.gaussian_kde(peak_2)
+
+    def plot_calibration(self, ax=None):
+        ax = super().plot_calibration(ax=ax)
+        ax.plot(self.bins, self.kde_2(self.bins), color=self.state_2_color)
+        ax.hist(self.peak_2, bins=self.bins, alpha=0.7, label=self.states[2], density=True, color=self.state_2_color)
+        ax.legend()
+        return ax
+
+    @cached_property
+    def params(self):
+        params = Parameters()
+        params.add("a", value=0, min=-0.1, max=1.1)
+        params.add("b", value=0, min=-0.1, max=1.1)
+        #params.add("c", value=0, min=0, max=1)
+        return params
+
+    def model_func(self, x, a, b):
+        c=1-a-b
+        return a*self.kde_0(x) + b * self.kde_1(x) + c * self.kde_2(x)
+
+    def get_z_magn_df(self, peak_df):
+        names = self.extract_names(peak_df)
+        z_magn = peak_df.peak_time.groupby(names).progress_apply(self.get_magn)
+        return z_magn.unstack()
